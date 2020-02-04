05719f775ebefb751de0ea1675728429d84cf7e7
Julian-Murillo <jomsk822@gmail.com>
2020-02-04 11:57:51 -0500

    added Build Controls folder with BuildControls and BuildControl components and corresponding css modules

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,27 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.module.css';
 
 const burger = (props) => {
+    // creates array of keys from passed object
+    let transformedIngredients = Object.keys(props.ingredients)
+        // convert from string to array with length of quantity of ingredients
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                // populate array with BurgerIngredient components
+                return <BurgerIngredient key={igKey + i} type={igKey} />;
+            });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
